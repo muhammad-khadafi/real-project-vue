f675b03c93845d70a52d224367ca21e5cb576df8
muhammad.khadafi <yusuf.khadafi@pusilkom.com>
2023-07-17 20:04:18 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app>
+    <v-app-bar
+        app
+        color="primary"
+        dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+        />
+
+        <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn rounded color="primary" dark class="mr-3" to="/">
+        Home
+      </v-btn>
+
+      <v-btn rounded color="primary" dark class="mr-3" to="/about">
+        About
+      </v-btn>
+
+      <v-btn rounded color="primary" dark class="mr-3" to="/contact">
+        Contact
+      </v-btn>
+
+      <v-btn rounded color="primary" dark to="/crud">
+        CRUD
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
